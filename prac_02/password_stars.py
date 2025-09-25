a77f8517658537a8869177994b1a70d85d71d6dd
dipro1 <dipro.bd10@gmail.com>
2025-09-26 00:01:12 +1000

    Add prac 2 initial password program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,12 +1,12 @@
 
-MINIMUM_LENGTH = 10
+MINIMUM_LENGTH = 10                                                             #set initial password requirements
 
-password = input("Please enter your password: ")
+password = input("Please enter your password: ")                                #get user password
 
-while len(password) < MINIMUM_LENGTH:
+while len(password) < MINIMUM_LENGTH:                                           #error correction
     print(f"Password needs to be at least {MINIMUM_LENGTH} character long")
 
-    password = input("Please enter your password: ")
+    password = input("Please enter your password: ")                            #get correct password
 
 
-print("*"*len(password))
+print("*"*len(password))                                                        #print hidden password
