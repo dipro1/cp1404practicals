92029ae4e5f15c96af449103f2cfb57e5fa213d4
dipro1 <dipro.bd10@gmail.com>
2025-09-27 20:36:54 +1000

    initial shell structure for sore menu

---
+++ prac_02/score_menu.py
@@ -0,0 +1,38 @@
+# #(G)et a valid score (must be 0-100 inclusive)
+# (P)rint result (copy or import your function to determine the result from score.py)
+# (S)how stars (this should print as many stars as the score)
+# (Q)uit
+
+
+
+def get_valid_score():
+
+
+
+
+def score_status(score):
+    if score < 0 or score > 100:
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+
+def show_stars(score):
+
+
+
+
+def main():
+
+    MENU ="""(G)et a valid score
+(P)rint result
+(S)how stars
+(Q)uit"""
+
+
+if __name__ == '__main__':
+    main()
