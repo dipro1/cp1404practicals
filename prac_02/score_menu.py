1679a218792e28ab330d2bcc5948aeb73b3d52f7
dipro1 <dipro.bd10@gmail.com>
2025-09-27 21:52:33 +1000

    add function to handel the input, score and print add main add exit text add comments

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,18 +1,20 @@
-# #(G)et a valid score (must be 0-100 inclusive)
-# (P)rint result (copy or import your function to determine the result from score.py)
-# (S)how stars (this should print as many stars as the score)
-# (Q)uit
 
 
 
-def get_valid_score():
+def get_valid_score():                                                          #get input from user
+
+    score = float(input("please input your score: "))                       #user input
+    if score < 0 or score > 100:                                #if else to get valid input
+        print("Please input a valid score between 0 and 100.")
+        return None
+    else:
+        return score
 
 
 
+def score_status(score):                                                #score status
 
-def score_status(score):
-    if score < 0 or score > 100:
-    elif score >= 90:
+    if score >= 90:
         return "Excellent"
     elif score >= 50:
         return "Passable"
@@ -20,19 +22,37 @@ def score_status(score):
         return "Bad"
 
 
-
-def show_stars(score):
-
-
+def show_stars(score):                                                  #print the stars
+    print('*' * int(score))
 
 
-def main():
+def main():                                                             #main
 
-    MENU ="""(G)et a valid score
+    choice = ""                                                         #initial choice
+    MENU = """Menu:
+(G)et a valid score
 (P)rint result
 (S)how stars
 (Q)uit"""
 
 
-if __name__ == '__main__':
+    while choice != "Q":                                                #choice for quit
+        print(MENU)                                                     #call menu
+        choice = input(">>> ").upper()                                  #set no case sensitivity
+
+        if choice == "G":                                               #choices
+            score = get_valid_score()
+        elif choice == "P":
+            result = score_status(score)
+            print(f"A score of {score} will be {result}")
+        elif choice == "S":
+            print(f"Score: {score}")
+            show_stars(score)
+        elif choice == "Q":
+            print("Alright, test's over. \033[1m*coughing*\033[0m. You can head on back to your desk.") #exit message with flare
+        else:
+            print("Please input a option from the menu")
+
+
+if __name__ == "__main__":
     main()
