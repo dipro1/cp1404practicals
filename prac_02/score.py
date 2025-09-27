99964b8cfd5f099d737e10b70834199c3e9ef9e3
dipro1 <dipro.bd10@gmail.com>
2025-09-27 19:52:21 +1000

    add functions to determine score status and user input and output program to determine score status

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,25 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+
+
+def score_status(score):                                        #function for
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+def main():
+
+    score = float(input("Enter score: "))
+    result = score_status(score)
+    print(result)
+
+if __name__ == '__main__':
+    main()
