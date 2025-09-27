f1c18a5db04547953c6c2a5a5dc082f9d96148db
dipro1 <dipro.bd10@gmail.com>
2025-09-27 20:10:40 +1000

    add random number generator for a random result and the corresponding status

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,8 +2,7 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
-
-
+import random
 
 
 def score_status(score):                                        #function for determining score status from user input
@@ -22,6 +21,9 @@ def main():                                                     #main function
     score = float(input("Enter score: "))                       #user input
     print(score_status(score))                                  #print the results fron score_status
 
+
+    random_score = random.randint(0,100)                 #generate random number inbetween 0 and 100
+    print(f"A score of {random_score} will be {score_status(random_score)}")    #print the random number and the status
 
 if __name__ == '__main__':
     main()                                                      #call main
