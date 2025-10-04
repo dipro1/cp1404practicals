2c046e713599b2abd1a8cd93349c225a71b2128e
dipro1 <dipro.bd10@gmail.com>
2025-10-05 02:17:35 +1000

    FIX solution 3 Add solution 4 and comments

--- prac_03/files.py
+++ prac_03/files.py
@@ -5,26 +5,36 @@
 
 user_name = input("Please input your name: ")
 
-file = open("name.txt","w")
-file.write(user_name)
-file.close()
+file = open("name.txt","w")                         #open file
+file.write(user_name)                               #write to file
+file.close()                                        #close file
 
 
 #2.
 
-file = open("name.txt","r")
-user_name = file.read()
-file.close()
+file = open("name.txt","r")                         #open file
+user_name = file.read()                             #read file
 
-print(f"Hi {user_name}!")
 
+print(f"Hi {user_name}!")                           #print from file
+file.close()                                        #colse file
 
 #3.
 
-file = open("numbers.txt","r")
-first_number = int(file.readline())
-second_number = int(file.readline())
+with open("numbers.txt","r") as file:               #open file
+    first_number = int(file.readline())             #read first line of file
+    second_number = int(file.readline())            #read second line of file
 
-total_number =  first_number + second_number
+total_number =  first_number + second_number        #add the numbers
 
-print(total_number)
+print(total_number)                                 #print the total
+
+
+#4.
+
+total_number = 0                                    #initial total
+with open("numbers.txt","r") as file:               #open file
+    for line in file:
+        total_number += int(line.strip())           #add the numbers one by one
+
+print(total_number)                                 #print total
