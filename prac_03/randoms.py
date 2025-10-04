8150b30247436c691e17f0ae3ff2cae348a0559b
dipro1 <dipro.bd10@gmail.com>
2025-10-04 22:24:11 +1000

    initial Q&A

---
+++ prac_03/randoms.py
@@ -0,0 +1,29 @@
+#What did you see on line 1?
+#prints random numbers in between 5 and 20
+
+
+#What was the smallest number you could have seen, what was the largest?
+#5
+
+
+#What did you see on line 2?
+#prints random number from 3 to 9 with intervals of 2
+
+#What was the smallest number you could have seen, what was the largest?
+#smallest - 2; largest - 9
+
+
+#Could line 2 have produced a 4?
+#no
+
+
+#What did you see on line 3?
+#prints random values inbetween 2.5 and 5.5 with varying decimal places
+
+
+#What was the smallest number you could have seen, what was the largest?
+#smallest - 2.55; largest - 5.5
+
+
+#Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+
