a910679a16f6738cb94501e0531acc964878cb30
dipro1 <dipro.bd10@gmail.com>
2025-10-09 22:24:13 +1000

    Initial code with answers

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,29 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# numbers[0]
+# 3
+# numbers[-1]
+# 2
+# numbers[3]
+# 1
+# numbers[:-1]
+# [3, 1, 4, 1, 5, 9]
+# numbers[3:4]
+# 1
+# 5 in numbers
+# true
+# 7 in numbers
+# false
+# "3" in numbers
+# false
+# numbers + [6, 5, 3]
+# [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+
+numbers[0] = "ten"  # replace first value to "ten"
+
+numbers[-1] = 1  # replace the last value to 1
+
+print(numbers[2:])  # print the list except the first 2 number
+
+print(9 in numbers)  # check if 9 is in the list
