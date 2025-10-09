bb285fe32254dbd05ace6806a3fcd96b0df4f83e
dipro1 <dipro.bd10@gmail.com>
2025-10-10 01:13:17 +1000

    Add username check

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,5 +1,15 @@
 def main():
     user_input_numbers = []
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']
+
+    user_name = input("please enter your username: ")
+
+    if user_name in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
 
     for i in range(5):
         numbers = int(input("number: "))
