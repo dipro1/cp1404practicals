618fc340e05bc03b9840a8e4f637a081bc041789
dipro1 <dipro.bd10@gmail.com>
2025-10-11 01:10:41 +1000

    Add comments

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -1,4 +1,3 @@
-
 """
 CP1404/CP5632 Practical
 Starter code for cumulative total income program
@@ -9,26 +8,26 @@ def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
     try:
-        number_of_months = int(input("How many months? "))
-        if number_of_months < 1:
-            print("Minimum number of months is 1")
+        number_of_months = int(input("How many months? "))  # get number of months
+        if number_of_months < 1:  # check for valid month
+            print("Minimum number of months is 1")  # error message for months less than 1
             return
     except ValueError:
-        print("Invalid input")
+        print("Invalid input")  # error message for month not a number
         return
 
     for month in range(1, number_of_months + 1):
         try:
-            income = float(input(f"Enter income for month {month}: "))          #change to fstring
+            income = float(input(f"Enter income for month {month}: "))  # get income of the month
             incomes.append(income)
-        except ValueError:
+        except ValueError:  # error check for income not a number
             print("invalid input")
             return
 
     print_report(incomes, number_of_months)
 
 
-def print_report(incomes,number_of_months):
+def print_report(incomes, number_of_months):
     print("\nIncome Report\n-------------")
     total = 0
     for month in range(1, number_of_months + 1):
