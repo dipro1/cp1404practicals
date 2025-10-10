fa27239d4668b1eb4d1ded65c033d82b2efb65f4
dipro1 <dipro.bd10@gmail.com>
2025-10-11 01:01:11 +1000

    Add error checking for pick number and comments

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -7,20 +7,24 @@ MAX = 45
 
 
 def main():
-    number_of_picks = int(input("How many quick picks? "))
+    try:
+        number_of_picks = int(input("How many quick picks? "))
+    except ValueError:  # error check
+        print("Invalid input please enter a number")  # error message
+        return
 
     for i in range(number_of_picks):
         quick_picks = generate_quick_picks()
         quick_picks.sort()  # Sort in ascending order
-        print(" ".join(f"{number:2}" for number in quick_picks))
+        print(" ".join(f"{number:2}" for number in quick_picks))  # print with spacing
 
 
 def generate_quick_picks():
-    pick = []
-    while len(pick) < NUMBERS_PER_PICK:
-        random_number = random.randint(MIN, MAX)
+    pick = []  # store picks
+    while len(pick) < NUMBERS_PER_PICK:  # check for length of pick vector
+        random_number = random.randint(MIN, MAX)  # get random numbers within range
         if random_number not in pick:
-            pick.append(random_number)
+            pick.append(random_number)  # append
     return pick
 
 
