9e118943733eb288e0f214a93df3c7fccb9f786b
dipro1 <dipro.bd10@gmail.com>
2025-10-11 00:49:59 +1000

    Add comments

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -4,25 +4,25 @@ def main():
                  'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
                  'bob']
 
-    user_name = input("please enter your username: ")
+    user_name = input("please enter your username: ")     # ask user for username
 
-    if user_name not in usernames:
+    if user_name not in usernames:                  #check for valid username
         print("Access denied")
         return
     else:
         print("Access granted\n")
 
-    for i in range(5):
-        numbers = int(input("number: "))
+    for i in range(5):                           #set range
+        numbers = int(input("number: "))        #get number in range
         user_input_numbers.append(numbers)
 
-    average = sum(user_input_numbers) / len(user_input_numbers)
+    average = sum(user_input_numbers) / len(user_input_numbers) #average
 
-    print(f"first number is {user_input_numbers[0]}")
-    print(f"The last number is {user_input_numbers[-1]}")
-    print(f"The smallest number is {min(user_input_numbers)}")
-    print(f"The largest number is {max(user_input_numbers)}")
-    print(f"The average of the numbers is {average}")
+    print(f"first number is {user_input_numbers[0]}")               #first number
+    print(f"The last number is {user_input_numbers[-1]}")           #last number
+    print(f"The smallest number is {min(user_input_numbers)}")      #min number
+    print(f"The largest number is {max(user_input_numbers)}")       #max number
+    print(f"The average of the numbers is {average}")               #average number
 
 
 main()
