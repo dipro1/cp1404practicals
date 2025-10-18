3b153a73f8a6de70863d2821997e0bab3dad0eda
dipro1 <dipro.bd10@gmail.com>
2025-10-19 02:06:44 +1000

    initial colour selection

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,12 @@
+COLOUR_CODES = {
+    "Absolute Zero": "#0048ba",
+    "Acid Green": "#b0bf1a",
+    "AliceBlue": "#f0f8ff",
+    "Alizarin Crimson": "#e32636",
+    "Amaranth": "#e52b50",
+    "Amber": "#ffbf00",
+    "Amethyst": "#9966cc",
+    "AntiqueWhite": "#faebd7",
+    "AntiqueWhite1": "#ffefdb",
+    "AntiqueWhite2": "#eedfcc"
+}
