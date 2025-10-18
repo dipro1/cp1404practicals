75fd3db3dc13ffa3e1e843394df1ce891c340cc0
dipro1 <dipro.bd10@gmail.com>
2025-10-19 09:48:31 +1000

    Add functions to split the email to get name and a function to display them

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,18 +1,24 @@
+
+
+
 def main():
 
 
+    email_to_name = {}
+    email = input("Email: ")
 
 
 
-
-def get_name:
-
-
-
-
-def display_email:
+def get_name(email):
+    prefix = email.split("@")[0]
+    parts = prefix.split(".")
+    name = " ".join(parts)
+    return name
 
 
+def display_email(email_to_name):
+    for email, name in email_to_name():
+        print(f"{name} {email}")
 
 if __name__ == "__main__":
-        main()
+    main()
