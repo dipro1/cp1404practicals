40b82c8ff285f82c94e8b3e9b789e7bd299d2140
dipro1 <dipro.bd10@gmail.com>
2025-10-19 10:01:30 +1000

    Add functions to get the name varification

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,14 +2,21 @@
 
 
 def main():
-
+    """Get mail and prompt varification and store the info"""
 
     email_to_name = {}
     email = input("Email: ")
-
-
+    while email != "":
+        name = get_name(email)
+        name_varify = input(f"Is your name {name}? (Y/n) ").lower()
+        if name_varify not in ("", "y"):
+            name = get_name(email)
+        email_to_name[email] = name
+        email = input("Email: ")
+    display_email(email_to_name)
 
 def get_name(email):
+    """Split the email to get the name"""
     prefix = email.split("@")[0]
     parts = prefix.split(".")
     name = " ".join(parts)
@@ -17,7 +24,8 @@ def get_name(email):
 
 
 def display_email(email_to_name):
-    for email, name in email_to_name():
+    """Display names and email"""
+    for email, name in email_to_name.items():
         print(f"{name} {email}")
 
 if __name__ == "__main__":
