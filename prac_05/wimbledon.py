d2865e6bba0c6ead09d278fbbd0a3b17bbb44e41
dipro1 <dipro.bd10@gmail.com>
2025-10-19 10:47:16 +1000

    Add function to extract info from the csv

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -8,10 +8,18 @@ Actual:  minutes
 FILENAME = "wimbledon.csv"
 
 def main():
+    records = load_data(FILENAME)
 
-def load_data():
+def load_data(filename):
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()
+        for line in in_file:
+            data = line.strip().split(",")
+    return data
 
-def displays_result():
+
+
+# def displays_result():
 
 
 
