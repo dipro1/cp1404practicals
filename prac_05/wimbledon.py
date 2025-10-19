45fa4eaf282afc5178b71d0ff3d9def4798478b9
dipro1 <dipro.bd10@gmail.com>
2025-10-19 10:36:56 +1000

    initial code

---
+++ prac_05/wimbledon.py
@@ -0,0 +1,19 @@
+"""
+CP1404Practical
+Wimbledon champions
+Estimate: 10 minutes
+Actual:  minutes
+"""
+
+FILENAME = "wimbledon.csv"
+
+def main():
+
+def load_data():
+
+def displays_result():
+
+
+
+if __name__ == "__main__":
+    main()
