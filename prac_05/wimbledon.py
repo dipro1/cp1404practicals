b67ddf942072fbdf36b92e1707cb3543214d33bb
dipro1 <dipro.bd10@gmail.com>
2025-10-19 11:06:43 +1000

    Add function to index the champion and country

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -6,9 +6,12 @@ Actual:  minutes
 """
 
 FILENAME = "wimbledon.csv"
-
+INDEX_CHAMPION = 1
+INDEX_COUNTRY= 2
 def main():
     records = load_data(FILENAME)
+
+
 
 def load_data(filename):
     with open(filename, "r", encoding="utf-8-sig") as in_file:
@@ -18,9 +21,21 @@ def load_data(filename):
     return data
 
 
+def process_records(data):
+    """Create dictionary of champions and set of countries from records (list of lists)."""
+    champion_count = {}
+    countries = set()
+    for record in data:
+        countries.add(record[INDEX_COUNTRY])
+        try:
+            champion_count[record[INDEX_CHAMPION]] += 1
+        except KeyError:
+            champion_count[record[INDEX_CHAMPION]] = 1
+    return champion_count, countries
 
 # def displays_result():
-
+#     print("Wimbledon champions: ")
+#     for
 
 
 if __name__ == "__main__":
