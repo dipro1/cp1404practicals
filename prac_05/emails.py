331892d56f4f9d5ac091faf65108e7fe3b09dd50
dipro1 <dipro.bd10@gmail.com>
2025-11-01 01:58:16 +1000

    Add fix to comments

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -2,20 +2,22 @@
 
 
 def main():
-    """Get mail and prompt varification and store the info"""
+    """Get mail and prompt verification and store the info"""
 
     email_to_name = {}
     email = input("Email: ")
     while email != "":
-        name = get_name(email)
-        name_varify = input(f"Is your name {name}? (Y/n) ").lower()
-        if name_varify not in ("", "y"):
-            name = get_name(email)
+        name = extract_name(email)
+        varify_name = input(f"Is your name {name}? (Y/n) ").lower()
+        if varify_name not in ("", "y","yes"):
+            name = extract_name(email)
+        else:
+            name = input("Email: ").strip().title()
         email_to_name[email] = name
         email = input("Email: ")
     display_email(email_to_name)
 
-def get_name(email):
+def extract_name(email):
     """Split the email to get the name"""
     prefix = email.split("@")[0]
     parts = prefix.split(".")
