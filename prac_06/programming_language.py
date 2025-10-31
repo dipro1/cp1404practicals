6af38942cbb171a3e717adea7e478074378b20ee
dipro1 <dipro.bd10@gmail.com>
2025-11-01 04:30:47 +1000

    Add class -> ProgrammingLanguage and __init__

---
+++ prac_06/programming_language.py
@@ -0,0 +1,9 @@
+from prac_04.list_comprehensions import names
+
+
+class ProgrammingLanguage:
+    def __init__(self, name, typing, reflection, year):
+        self.name = names
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
