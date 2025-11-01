cc2732b4610fca4d883c6669b32bffb6b921957b
dipro1 <dipro.bd10@gmail.com>
2025-11-02 00:38:36 +1000

    Add dockstring

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,23 +1,26 @@
 """
 Estimate: 30 minutes
 Start time:  12:04 am
-Finish time: 4:56 am
+Finish time: 12:38 am
 """
-YEAR = 2025
+YEAR = 2025 #current year
 
 class Guitar:
-
+    """guitar class with 3 variables"""
     def __init__(self, name = "", year = 0, cost = 0.0):
-
+        """variables for the class"""
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
+        """return the info of the guitar"""
         return f"{self.name} ({self.year}) : ${self.cost}"
 
     def get_age(self):
+        """check for age of guitar"""
         return YEAR - self.year
 
     def is_vintage(self):
+        """check if guitar is vintage"""
         return self.get_age() >= 50
