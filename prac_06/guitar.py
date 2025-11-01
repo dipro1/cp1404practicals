15e1b069d6a883b911f20fc033ba2578e82dce50
dipro1 <dipro.bd10@gmail.com>
2025-11-02 00:08:10 +1000

    Add class -> guitar and attributes

---
+++ prac_06/guitar.py
@@ -0,0 +1,13 @@
+"""
+Estimate: 30 minutes
+Start time:  12:04 am
+Finish time: 4:56 am
+"""
+
+class Guitar:
+
+    def __init__(self, name = "", year = 0, cost = 0.0):
+
+        self.name = name
+        self.year = year
+        self.cost = cost
