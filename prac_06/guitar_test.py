9f4f56d04d0588b14dade6882e94e70ac6fb3e77
dipro1 <dipro.bd10@gmail.com>
2025-11-02 01:07:10 +1000

    Add test for 2 guitar vintage status

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -18,7 +18,10 @@ def main():
     print(f"{gibson.name} get_age() - Expected {expected_gibson_age}. Got {gibson.get_age()}")
     print(f"{another.name} get_age() - Expected {expected_another_age}. Got {another.get_age()}")
 
-
+    expected_gibson_vintage = expected_gibson_age >= 50
+    expected_another_vintage = expected_gibson_age >= 50
+    print(f"{gibson.name} is_vintage() - Expected {expected_gibson_vintage}. Got {gibson.is_vintage()}")
+    print(f"{another.name} is_vintage() - Expected {expected_another_vintage}. Got {another.is_vintage()}")
 
 if __name__ == "__main__":
     main()
