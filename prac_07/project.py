028c01994192f2baee9d858f5c7720329e810969
dipro1 <dipro.bd10@gmail.com>
2025-11-09 13:00:35 +1000

    Add __lt__, and functions

--- prac_07/project.py
+++ prac_07/project.py
@@ -9,3 +9,28 @@ class Project:
         self.priority = priority
         self.cost_estimate = cost_estimate
         self.percent_complete = percent_complete
+
+    def __lt__(self, other: "Project") -> bool:
+        """sort by less than"""
+        if self.priority != other.priority:
+            return self.priority < other.priority
+        if self.start_date != other.start_date:
+            return self.start_date < other.start_date
+        return self.name < other.name
+
+    def check_if_complete(self) -> bool:
+        """check if project is complete"""
+        return self.percent_complete >= 100
+
+    def check_cutoff(self, d: date) -> bool:
+        """check if cutoff date is passed"""
+        return self.start_date > d
+
+    def check_start_date(self) -> str:
+        """day/month/year"""
+        return self.start_date.strftime("%d/%m/%Y")
+
+    def __str__(self) -> str:
+        return (f"{self.name}, start: {self.check_start_date()}, "
+                f"priority {self.priority}, estimate: ${self.cost_estimate:,.2f}, "
+                f"completion: {self.percent_complete}%")
