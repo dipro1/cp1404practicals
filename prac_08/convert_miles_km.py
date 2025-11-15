b4057b200a224ce506f8771267e355ac1629345f
dipro1 <dipro.bd10@gmail.com>
2025-11-16 03:17:48 +1000

    Add up and down button uses

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,6 +1,6 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.core.window import Window
+
 from kivy.properties import StringProperty
 
 
@@ -19,9 +19,17 @@ class ConvertMilesKmApp(App):
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
+    def miles(self):
+        return float(self.root.ids.miles_input.text)
+
+
     def results(self):
-        miles = float(self.root.ids.miles_input.text)
+        miles = self.miles()
         km = miles * MILES_TO_KM
         self.output_km = f"{km: }"
+
+    def handel_change(self, change):
+        new_miles = self.miles() + change
+        self.root.ids.miles_input.text = str(new_miles)
 
 ConvertMilesKmApp().run()
