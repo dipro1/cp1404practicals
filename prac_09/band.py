e106d441cf9c3c57843ff7bdbabf0ffab788017b
dipro1 <dipro.bd10@gmail.com>
2025-11-23 02:59:36 +1000

    Add str to get the names of the musicians

--- prac_09/band.py
+++ prac_09/band.py
@@ -3,6 +3,9 @@ class Band:
         self.name = name
         self.musicians = []
 
+    def __str__(self):
+        musicians_text = ", ".join(str(musician) for musician in self.musicians)
+        return f"{self.name} ({musicians_text})"
 
     def add(self, musician):
         self.musicians.append(musician)
