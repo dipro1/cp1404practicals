f0d75ee4771ae8d02d28a3b32028bd9d7a105fee
dipro1 <dipro.bd10@gmail.com>
2025-11-23 02:45:46 +1000

    Add round to round the int to the nearest 1

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -6,10 +6,11 @@ def main():
 
     fancy_taxi.drive(18)
 
-    expected_fare = 48.78
+    expected_fare = 48.80
     actual_fare = fancy_taxi.get_fare()
     assert f"{actual_fare:.2f}" == f"{expected_fare:.2f}"
 
+    print(f"expected_fare: {expected_fare}" f" and the actual fare is {actual_fare}" )
     print("The fare is fair")
 
 
