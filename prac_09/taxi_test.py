7fbc19176c38bda77aacdf7e7dec5fd8b934a2a7
dipro1 <dipro.bd10@gmail.com>
2025-11-22 21:39:36 +1000

    Add restart meter and go a 100km

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -9,6 +9,13 @@ def main():
     print(my_taxi)
     print(f"Current fare is ${my_taxi.get_fare()}")
 
+    my_taxi.start_fare()
+
+    my_taxi.drive(100)
+
+    print(my_taxi)
+    print(f"Current fare is ${my_taxi.get_fare()}")
+    # have to change the initial fuel of my_taxi so that it can go to 100km it only does 100km odo
 
 if __name__ == "__main__":
     main()
