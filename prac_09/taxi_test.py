3583c0d8c4e9a62f3195526f9b2f39377e6f02f2
dipro1 <dipro.bd10@gmail.com>
2025-11-22 21:14:56 +1000

    Add drive to get

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -2,8 +2,10 @@ from taxi import Taxi
 
 def main():
     my_taxi = Taxi("Prius 1", 100, 1.23)
-    print(my_taxi)  #test
 
+
+    my_taxi.drive(40)
+    print(f"{my_taxi.get_fare()}") #test
 
 
 if __name__ == "__main__":
