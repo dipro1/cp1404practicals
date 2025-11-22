fa8cf55e8d24f6b6ad6efa1ab5be3c56f2d7107c
dipro1 <dipro.bd10@gmail.com>
2025-11-23 02:07:44 +1000

    Add str to print

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -2,7 +2,6 @@ from taxi import Taxi
 
 
 class SilverServiceTaxi(Taxi):
-
     flagfall = 4.50
 
     def __init__(self, name, fuel, fanciness):
@@ -13,3 +12,6 @@ class SilverServiceTaxi(Taxi):
     def get_fare(self):
         base_fare = super().get_fare()
         return base_fare + self.flagfall
+
+    def __str__(self):
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:}"
--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -2,7 +2,7 @@ from silver_service_taxi import SilverServiceTaxi
 
 
 def main():
-    fancy_taxi = SilverServiceTaxi("Fancy Taxi", 100)
+    fancy_taxi = SilverServiceTaxi("Fancy Taxi", 100, 4)
     print(fancy_taxi)
 
 
