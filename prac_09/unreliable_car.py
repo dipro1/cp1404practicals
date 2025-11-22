c3b98733bd91f7165be239123938023a654f06d1
dipro1 <dipro.bd10@gmail.com>
2025-11-22 23:20:08 +1000

    Add drive method

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -1,7 +1,16 @@
+import random
+
 from car import Car
+
 
 class UnreliableCar(Car):
 
     def __init__(self, name, fuel, reliability):
         super().__init__(name, fuel)
         self.reliability = reliability
+
+    def drive(self, distance):
+        random_number = random.uniform(0, 100)
+        if random_number < self.reliability:
+            return super().drive(distance)
+        return 0
