8523abe53dc8b65fc7792df6d1cbfe3484a31e5c
dipro1 <dipro.bd10@gmail.com>
2025-11-23 03:28:25 +1000

    Add list of taxis and current taxi

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -6,6 +6,9 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 def main():
     print("Let's drive!")
 
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
+
     print(MENU)
     choice = input(">>> ").lower()
     while choice != "q":
