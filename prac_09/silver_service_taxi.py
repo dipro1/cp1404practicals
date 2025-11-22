d0585db00dc11d12f48e4f965a0b3890f50e8a40
dipro1 <dipro.bd10@gmail.com>
2025-11-23 01:58:35 +1000

    Add class variable

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -2,6 +2,9 @@ from taxi import Taxi
 
 
 class SilverServiceTaxi(Taxi):
+
+    flagfall = 4.50
+
     def __init__(self, name, fuel, fanciness):
         super().__init__(name, fuel)
         self.fanciness = fanciness
