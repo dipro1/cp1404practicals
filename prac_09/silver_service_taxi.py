e558e292ca261a1c07f5b4a04f2b84605383f60a
dipro1 <dipro.bd10@gmail.com>
2025-11-23 01:57:27 +1000

    Add fanciness as an attribute

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -2,4 +2,7 @@ from taxi import Taxi
 
 
 class SilverServiceTaxi(Taxi):
-    pass
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km = Taxi.price_per_km * fanciness
